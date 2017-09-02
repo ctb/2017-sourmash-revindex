bef968bde350ab50a5bb8393043dc140bb45ad40
Titus Brown <titus@idyll.org>
2017-09-02 13:34:02 -0400

    support multiple LCA databases for classify-common-hahes

--- classify-common-hashes.py
+++ classify-common-hashes.py
@@ -15,12 +15,16 @@ def main():
     p = argparse.ArgumentParser()
     p.add_argument('-k', '--ksize', default=31, type=int)
     p.add_argument('revindex')
-    p.add_argument('lca_json')
+    p.add_argument('lca_json', nargs='+')
     args = p.parse_args()
 
     idx = revindex_utils.HashvalRevindex(args.revindex)
-    lca_db = lca_json.LCA_Database(args.lca_json)
-    taxfoo, hashval_to_lca, scaled = lca_db.get_database(args.ksize, SCALED)
+
+    lca_db_list = []
+    for lca_filename in args.lca_json:
+        lca_db = lca_json.LCA_Database(lca_filename)
+        taxfoo, hashval_to_lca, _ = lca_db.get_database(args.ksize, SCALED)
+        lca_db_list.append((taxfoo, hashval_to_lca))
     
     cnt = collections.Counter()
     for k, v in idx.hashval_to_sigids.items():
@@ -33,12 +37,24 @@ def main():
         if c < 10:              # break when we hit things in < 10 samples.
             break
         total += 1
-        lca = hashval_to_lca.get(k)
-        if lca:
-            print('hash {}, in {} samples; lineage: {}'.format(k, c, ";".join(taxfoo.get_lineage(lca))))
-            found += 1
-        else:
+        lca_set = set()
+
+        for (_, hashval_to_lca) in lca_db_list:
+            this_lca = hashval_to_lca.get(k)
+            if this_lca is not None:
+                lca_set.add(this_lca)
+
+        if not lca_set:
             unknown[c] += 1
+            continue
+
+        assert lca_set, lca_set
+
+        lca = taxfoo.find_lca(lca_set)
+        assert(lca), lca
+
+        print('hash {}, in {} samples; lineage: {}'.format(k, c, ";".join(taxfoo.get_lineage(lca))))
+        found += 1
 
     print('found', found, 'of', total)
     print('distribution of unknowns:')
