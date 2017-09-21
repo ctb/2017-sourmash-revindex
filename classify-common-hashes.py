8ef4ba877518c8b4fef95ccbfd2e963f05b07895
C. Titus Brown <titus@idyll.org>
2017-09-21 14:02:48 -0700

    add --sample-threshold and --abundance-threshold

--- classify-common-hashes.py
+++ classify-common-hashes.py
@@ -9,11 +9,18 @@ import revindex_utils
 import lca_json
 
 SCALED=100000
+DEFAULT_KSIZE=31
+DEFAULT_SAMPLE_THRESHOLD=10
+DEFAULT_ABUND_THRESHOLD=1
 
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('-k', '--ksize', default=31, type=int)
+    p.add_argument('-k', '--ksize', default=DEFAULT_KSIZE, type=int)
+    p.add_argument('--sample-threshold',
+                   default=DEFAULT_SAMPLE_THRESHOLD, type=int)
+    p.add_argument('--abundance-threshold',
+                   default=DEFAULT_ABUND_THRESHOLD, type=int)
     p.add_argument('revindex')
     p.add_argument('lca_json', nargs='+')
     args = p.parse_args()
@@ -27,25 +34,27 @@ def main():
         lca_db_list.append((taxfoo, hashval_to_lca))
     
     cnt = collections.Counter()
-    for k, v in idx.hashval_to_sigids.items():
-        cnt[k] += len(v)
+    for k, v in idx.hashval_to_abunds.items():
+        cnt[k] += len([abund for abund in v \
+                       if abund >= args.abundance_threshold])
 
     total = 0
     found = 0
     unknown = collections.defaultdict(int)
-    for k, c in cnt.most_common():
-        if c < 10:              # break when we hit things in < 10 samples.
+    for hashval, count in cnt.most_common():
+        # break when we hit things in < 10 samples.        
+        if count < args.sample_threshold:
             break
         total += 1
         lca_set = set()
 
         for (_, hashval_to_lca) in lca_db_list:
-            this_lca = hashval_to_lca.get(k)
+            this_lca = hashval_to_lca.get(hashval)
             if this_lca is not None:
                 lca_set.add(this_lca)
 
         if not lca_set:
-            unknown[c] += 1
+            unknown[count] += 1
             continue
 
         assert lca_set, lca_set
@@ -53,7 +62,7 @@ def main():
         lca = taxfoo.find_lca(lca_set)
         assert(lca), lca
 
-        print('hash {}, in {} samples; lineage: {}'.format(k, c, ";".join(taxfoo.get_lineage(lca))))
+        print('hash {}, in {} samples; lineage: {}'.format(hashval, count, ";".join(taxfoo.get_lineage(lca))))
         found += 1
 
     print('found', found, 'of', total)
