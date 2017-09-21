09986e70f49a3554ef465384db69cc3ee530db52
C. Titus Brown <titus@idyll.org>
2017-09-21 13:54:01 -0700

    add get_sourmash_signature

--- revindex_utils.py
+++ revindex_utils.py
@@ -1,10 +1,25 @@
 #! /usr/bin/env python
 from pickle import load
+import sourmash_lib.signature
 
 class HashvalRevindex(object):
     def __init__(self, savename):
         x = load_revindex(savename)
         self.hashval_to_sigids, self.sigid_to_siginfo, self.hashval_to_abunds = x
+
+    def get_siginfo(self, hashval):
+        for siginfo_id in self.hashval_to_sigids.get(hashval, []):
+            yield self.sigid_to_siginfo[siginfo_id]
+
+
+def get_sourmash_signature(filename, md5):
+    "Get sourmash signature based on siginfo."
+    sigiter = sourmash_lib.signature.load_signatures(filename)
+    for sig in sigiter:
+        if sig.md5sum() == md5:
+            return sig
+
+    raise Exception('no such sig: {}:{}'.format(filename, md5))
 
 
 def load_revindex(savename):
